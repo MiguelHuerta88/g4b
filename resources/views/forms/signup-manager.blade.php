21352e189eaeccefbc893db72c6a55b03def52c2
Miguel Huerta <miguelhuerta@Miguels-MBP.attlocal.net>
2018-10-22 19:51:46 -0700

    Signup process started. Many updates

--- resources/views/forms/signup-manager.blade.php
+++ resources/views/forms/signup-manager.blade.php
@@ -1,7 +1,1 @@
-{!! Form::open(['url' => route('signup.post'), 'id' => 'signupForm']) !!}
-	@include('forms.signup-common')
-
 	{{-- user managed here --}}
-
-		<button type="submit" class="btn btn-primary">Create</button>
-{!! Form::close() !!}
--- resources/views/pages/signup.blade.php
+++ resources/views/pages/signup.blade.php
@@ -15,29 +15,32 @@
 	<div class='form col-lg-10'>
 		<ul class='nav nav-tabs col-lg-5' id='form-tabs'>
 			<li class='nav-item' id='artist'>
-				<a class='nav-link active' href="#artist">Artist</a>
+				<a class='nav-link active' href="#">Artist</a>
 			</li>
 
 			<li class='nav-item' id='manager'>
-				<a class='nav-link' href="#manager">Manager</a>
+				<a class='nav-link' href="#">Manager</a>
 			</li>
 
 			<li class='nav-item' id='coordinator'>
-				<a class='nav-link' href="#coordinator">Event Coordinator</a>
+				<a class='nav-link' href="#">Event Coordinator</a>
 			</li>
 		</ul>
 		<div class='form-group'>
-			<div class='active' id='artist-form'>
-				@include('forms.signup-artist', ['genres' => $genres])
-			</div>
+			{!! Form::open(['url' => route('signup.post'), 'id' => 'signupForm']) !!}
+				@include('forms.signup-common')
+				<div class='active' id='artist-form'>
+					@include('forms.signup-artist', ['genres' => $genres])
+				</div>
 
-			<div class='hide' id='manager-form'>
-				@include('forms.signup-manager')
-			</div>
+				<div class='hide' id='manager-form'>
+					@include('forms.signup-manager')
+				</div>
 
-			<div class='hide' id='coordinator-form'>
-				events
-			</div>
+				<div class='hide' id='coordinator-form'>
+					events
+				</div>
+			{!! Form::close() !!}
 		</div>
 	</div>
 @endsection
