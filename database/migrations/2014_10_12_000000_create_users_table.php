09a6a06691c109e14248f10fa7475558265fa4c3
Miguel Huerta <miguel.huerta@internetbrands.com>
2018-10-24 09:06:18 -0700

    made band name unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('username')->unique();
             $table->string('password');
-            $table->string('artist_name');
+            $table->string('artist_name')->unique();
             $table->string('hometown');
             $table->string('state');
             $table->integer('genre_id')->nullable();
